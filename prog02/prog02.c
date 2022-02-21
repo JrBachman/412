c9833e5b2407b302bdbc1931036567f5bf6d3609
Jason Bachman <Jbachman@uri.edu>
2022-02-21 00:00:05 -0500

    Got prog02.c to work

---
+++ prog02/prog02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+
+int main(int argc, const char* argv[])
+{
+	//	usual test of argc
+    int counter = 0;
+	char word[50];
+    char target[50];
+    strcpy(target, argv[1]);
+    FILE *in_file;
+    
+    
+    for (int i = 2; i < argc; i++){
+        in_file = fopen(argv[i], "r");
+        while(fscanf(in_file, "%s", word) != EOF){
+            //printf("%s\n", word);
+            if(strcmp(word, target) == 0){
+                counter++;
+            }
+        }
+        fclose(in_file);
+        
+    }
+    printf("%d", counter);
+    return 0;
+    
+
+}
